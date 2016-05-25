f47d1f8bd622d208b7b36a1f7ddb2081ad85d237
paulief <paulfagan1991@gmail.com>
2016-05-25 09:49:06 -0700

    Update tests for HTTP helper to match new functionality

--- spec/helpers/http_helper_spec.rb
+++ spec/helpers/http_helper_spec.rb
@@ -1,26 +1,49 @@
-require_relative '../../app/helpers/http_helper'
+require 'rails_helper'
+
+class MockResponse
+	def initialize(code, body)
+		@code = code
+		@body = body
+	end
+
+	def code
+		@code
+	end
+
+	def body
+		@body
+	end
+
+	def to_s
+		{ code: @code, body: @body }.to_json
+	end
+end
 
 RSpec.describe 'HttpHelper' do
 	describe '.get' do
 		before(:each) do
 			allow(HTTP).to receive(:headers).and_return(HTTP)
-			allow(HTTP).to receive(:get).and_return({ code: 200, body: { poster: 'poster_url.jpg' } })
+			allow(HTTP).to receive(:get).and_return(MockResponse.new(200, { poster: 'poster.jpg' }))
 		end
 
-		context 'when passed parameters' do
+		context 'when passed simple parameters' do
 			it 'should call get on HTTP with the correct query string' do
+				HttpHelper.get({ title: 'Interstellar', year: '2015' })
+				expect(HTTP).to have_received(:get).with('https://community-netflix-roulette.p.mashape.com/api.php?title=Interstellar&year=2015')
+			end
+		end
+
+		context 'when there is a space in the movie title' do
+			it 'should call get on HTTP with the space correctly replaced with +' do
 				HttpHelper.get({ title: 'The Shining', year: '1980' })
 				expect(HTTP).to have_received(:get).with('https://community-netflix-roulette.p.mashape.com/api.php?title=The+Shining&year=1980')
 			end
 		end
 
-		context 'when the request fails' do
-			before(:each) do
-				allow(HTTP).to receive(:get).and_return({ code: 400 })
-			end
-
-			it 'should raise an error' do
-				expect{ HttpHelper.get({ title: 'Movie', year: '1991' })}.to raise_error('Bad Request')
+		context 'when there is an escapable character other than space in the movie title' do
+			it 'should call get on HTTP with the character escaped' do
+				HttpHelper.get({ title: 'The " Movie', year: '2006' })
+				expect(HTTP).to have_received(:get).with('https://community-netflix-roulette.p.mashape.com/api.php?title=The+%22+Movie&year=2006')
 			end
 		end
 	end
