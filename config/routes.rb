38fa71b05c1f6f1dafad0a89d5fc36857db248f2
paulief <paulfagan1991@gmail.com>
2016-05-25 11:27:10 -0700

    Redirect / to new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :movie_requests, only: [:new, :create, :show]
   get '/movie_requests', to: redirect('movie_requests/new')
+  get '/', to: redirect('movie_requests/new')
 end
