f908552fb886468f26c5c20b39364ec932d7cce1
paulief <paulfagan1991@gmail.com>
2016-05-25 10:37:21 -0700

    Fix deprecated rspec methods

--- spec/models/movie_request_spec.rb
+++ spec/models/movie_request_spec.rb
@@ -4,25 +4,25 @@ RSpec.describe 'MovieRequest' do
 	describe 'validations' do
 		context 'when title, year, and poster are present' do
 			it 'should be valid' do
-				MovieRequest.new(title: 'Title', year: 'Year', poster: 'poster.jpg').should be_valid
+				expect(MovieRequest.new(title: 'Title', year: 'Year', poster: 'poster.jpg')).to be_valid
 			end
 		end
 
 		context 'when title is missing' do
 			it 'should be valid' do
-				MovieRequest.new(year: 'Year', poster: 'poster.jpg').should_not be_valid
+				expect(MovieRequest.new(year: 'Year', poster: 'poster.jpg')).to_not be_valid
 			end
 		end
 
 		context 'when year is missing' do
 			it 'should be valid' do
-				MovieRequest.new(title: 'Title', poster: 'poster.jpg').should_not be_valid
+				expect(MovieRequest.new(title: 'Title', poster: 'poster.jpg')).to_not be_valid
 			end
 		end
 
 		context 'when poster is missing' do
 			it 'should be valid' do
-				MovieRequest.new(title: 'Title', year: 'Year').should_not be_valid
+				expect(MovieRequest.new(title: 'Title', year: 'Year')).to_not be_valid
 			end
 		end
 	end
